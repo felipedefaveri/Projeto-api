3c1863e151238cc7717c600cffe51adbc155a02c
Giulia <giu.olivers@gmail.com>
2021-11-11 20:02:52 -0300

    Atualiza tratamento de exceções

--- src/main/java/org/serratec/ecommerce/exception/ControllerExceptionHandler.java
+++ src/main/java/org/serratec/ecommerce/exception/ControllerExceptionHandler.java
@@ -1,5 +1,27 @@
 package org.serratec.ecommerce.exception;
 
-public class ControllerExceptionHandler {
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		List<String> erros = new ArrayList<String>();
+		
+		for (FieldError field : ex.getBindingResult().getFieldErrors()) {
+			erros.add(field.getField() +" - " + field.getDefaultMessage());						
+		}
+		
+		ErroResposta erroResposta = new ErroResposta(status.value(), "Existem campos inválidos", LocalDateTime.now(), erros);
+		return super.handleExceptionInternal(ex, erroResposta, headers, status, request);
+	}
 }
