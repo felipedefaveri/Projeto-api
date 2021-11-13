a14875dfe1a09fd88639911cad516ec115649f02
Giulia <giu.olivers@gmail.com>
2021-11-13 13:20:47 -0300

    Atualiza ClienteService

--- src/main/java/org/serratec/ecommerce/service/ClienteService.java
+++ src/main/java/org/serratec/ecommerce/service/ClienteService.java
@@ -16,63 +16,70 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ClienteService {
-	@Autowired
-	private ClienteRepository clienteRepository;
-	
-	@Autowired
-	private BCryptPasswordEncoder passwordEncoder;
+    
+    @Autowired
+    private ClienteRepository clienteRepository;
+    
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
-	public List<ClienteLogadoDTO> listar(){
-		List<ClienteLogadoDTO> clientesDTO = new ArrayList<ClienteLogadoDTO>();
-		List<Cliente> clientes = clienteRepository.findAll();
-		
-		for(Cliente cliente : clientes) {
-			ClienteLogadoDTO clienteDTO = new ClienteLogadoDTO(cliente);
-			clientesDTO.add(clienteDTO);
-		}
-		return clientesDTO;
+    public List<ClienteLogadoDTO> listar(){
+        List<ClienteLogadoDTO> clientesDTO = new ArrayList<ClienteLogadoDTO>();
+        List<Cliente> clientes = clienteRepository.findAll();
+        
+        for(Cliente cliente : clientes) {
+            ClienteLogadoDTO clienteDTO = new ClienteLogadoDTO(cliente);
+            clientesDTO.add(clienteDTO);
+        }
+        return clientesDTO;
   }
 
-	public List<Cliente> obterTodos() {
-		return clienteRepository.findAll();
-	}
+    public List<Cliente> obterTodos() {
+        return clienteRepository.findAll();
+    }
 
-	public Optional<Cliente> buscar(Long id) {
-		return clienteRepository.findById(id);
-	}
+    public Optional<Cliente> buscar(Long id) {
+        return clienteRepository.findById(id);
+    }
 
-	public ClienteLogadoDTO criar(ClienteLogadoDTO clienteLogadoDTO) throws EmailException {
-		
-		if (clienteRepository.findByEmail(clienteLogadoDTO.getEmail()) != null) {
-			throw new EmailException("E-mail existente.");
-		}
-		
-		Cliente cliente = new Cliente();
-		cliente.setNome(clienteLogadoDTO.getNome());
-		cliente.setEmail(clienteLogadoDTO.getEmail());
-		cliente.setSenha(passwordEncoder.encode(clienteLogadoDTO.getSenha()));
-		cliente = clienteRepository.save(cliente);
-		
-		return new ClienteLogadoDTO(cliente);
-	}
-	
-	public Cliente criar(@Valid Cliente cliente) {
-		return clienteRepository.save(cliente);
-	}
+    public ClienteLogadoDTO criar(ClienteLogadoDTO clienteLogadoDTO) throws EmailException {
+        
+        if (clienteRepository.findByEmail(clienteLogadoDTO.getEmail()) != null) {
+            throw new EmailException("E-mail existente.");
+        }
+        Cliente cliente = new Cliente();
+            cliente.setNome(clienteLogadoDTO.getNome());
+            cliente.setSobrenome(clienteLogadoDTO.getSobrenome());
+            cliente.setDataNascimento(clienteLogadoDTO.getDataNascimento());
+            cliente.setEmail(clienteLogadoDTO.getEmail());
+            cliente.setCpf(clienteLogadoDTO.getCpf());
+            cliente.setSenha(passwordEncoder.encode(clienteLogadoDTO.getSenha()));
+            cliente = clienteRepository.save(cliente);
+        
+        return new ClienteLogadoDTO(cliente);
+    }
 
-	public Cliente atualizar(Long id, @Valid Cliente cliente) {
-		if (!clienteRepository.existsById(id)) {
-			return null;
-		}
-		cliente.setId(id);
-		return clienteRepository.save(cliente);
-	}
+    public ClienteLogadoDTO atualizar(Long id, @Valid ClienteLogadoDTO clienteLogadoDTO) {
+        if (!clienteRepository.existsById(id)) {
+            return null;
+        }
+        Cliente cliente = new Cliente();
+        cliente.setNome(clienteLogadoDTO.getNome());
+        cliente.setSobrenome(clienteLogadoDTO.getSobrenome());
+        cliente.setDataNascimento(clienteLogadoDTO.getDataNascimento());
+        cliente.setEmail(clienteLogadoDTO.getEmail());
+        cliente.setCpf(clienteLogadoDTO.getCpf());
+        cliente.setSenha(passwordEncoder.encode(clienteLogadoDTO.getSenha()));
+        cliente = clienteRepository.save(cliente);
+    
+    return new ClienteLogadoDTO(cliente);
+    }
 
-	public boolean deletar(Long id) {
-		if(clienteRepository.existsById(id)) {
-			clienteRepository.deleteById(id);
-			return true;			
-		}
-		return false;
-	}
+    public boolean deletar(Long id) {
+        if(clienteRepository.existsById(id)) {
+            clienteRepository.deleteById(id);
+            return true;            
+        }
+        return false;
+    }
 }
